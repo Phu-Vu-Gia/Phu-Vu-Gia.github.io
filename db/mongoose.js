8fc3a8ce216c8a86d1e7a1a37f91a52eabc7d614
Phu-Vu-Gia <s4132254@rmit.edu.vn>
2026-01-06 09:51:17 +0700

    edit the mongo uri to env version

--- db/mongoose.js
+++ db/mongoose.js
@@ -4,9 +4,7 @@ require('dotenv').config();
 // Provide your MongoDB Atlas connection string
 // Make sure to connect to the DB named 2025b_final_sid
 
-MONGODB_CONNECTION_STRING = 'mongodb+srv://<username>:<password>@<host>/2025b_final_<sid>?retryWrites=true&w=majority&appName=Cluster0'
-
-mongoose.connect(MONGODB_CONNECTION_STRING)
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING)
         .then(() => {console.log("Connected to MongoDB Atlas")})
         .catch(error => {console.error(error)});
 
